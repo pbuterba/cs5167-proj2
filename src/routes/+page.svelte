e90ed76a35bbdf7bc47fcb19295cdd7bbd82327f
madicoulson <coulsomj@mail.uc.edu>
2024-10-09 14:27:27 -0400

    update naming convention

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <div>
 	<a href="component-examples">Go to component examples</a>
 	<br>
-	<a href="generate-outfit">Go to Generate Outfit</a>
+	<a href="choose-outfit">Go to Choose Outfit</a>
 </div>
