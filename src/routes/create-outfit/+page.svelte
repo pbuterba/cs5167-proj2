7b2188b4f5457fb7e07137d37e7e86d950edd901
bellettichloe <belletce@mail.uc.edu>
2024-10-14 18:50:39 -0400

    big things happening

--- src/routes/create-outfit/+page.svelte
+++ src/routes/create-outfit/+page.svelte
@@ -3,18 +3,15 @@
 	import AccordionBody from '$lib/components/Accordion/AccordionBody.svelte';
 	import AccordionHeader from '$lib/components/Accordion/AccordionHeader.svelte';
 	import Button from '$lib/components/Button/Button.svelte';
-	import Checkbox from '$lib/components/Checkbox/Checkbox.svelte';
-	import Flyout from '$lib/components/Flyout/Flyout.svelte';
-	import Header from '$lib/components/Header/Header.svelte';
-	import Tag from '$lib/components/Tag/Tag.svelte';
-	import Text from '$lib/components/Text/Text.svelte';
 	import TextInput from '$lib/components/TextInput/TextInput.svelte';
 	import { clothesStore, outfitStore } from '$lib/utilities/stores.js';
 	import { onMount } from 'svelte';
 	import Tabs from '$lib/components/Tabs/Tabs.svelte';
 	import TabContent from '$lib/components/Tabs/TabContent.svelte';
-	import TabLabel from '$lib/components/Tabs/TabLabel.svelte'; 
-
+	import TabLabel from '$lib/components/Tabs/TabLabel.svelte';
+	import { get, writable } from 'svelte/store';
+	import Header from '$lib/components/Header/Header.svelte';
+	import Text from '$lib/components/Text/Text.svelte';
 	let shirts = [];
 	let pants = [];
 	let currentShirtIndex = 0;
@@ -22,18 +19,37 @@
 	let selectedShirt = null;
 	let selectedPant = null;
 	let outfitName = '';
+	let savedOutfits = get(outfitStore);
 
 	// Load all available shirts and pants when the component is mounted
 	onMount(() => {
-		shirts = clothesStore.getClothingItemsByType("top");
-		pants = clothesStore.getClothingItemsByType("bottom");
+		// Initialize shirts and pants from clothesStore
+		shirts = clothesStore.getClothingItemsByType('top');
+		pants = clothesStore.getClothingItemsByType('bottom');
 
+		// Select the first shirt and pant
 		if (shirts.length) {
 			selectedShirt = shirts[0];
 		}
 		if (pants.length) {
 			selectedPant = pants[0];
 		}
+
+		// Subscribe to the outfits store to display saved outfits
+		outfitStore.subscribe((outfits) => {
+			savedOutfits = outfits.map((outfit) => {
+				const shirt = clothesStore.getClothingItemById(outfit.topid);
+				const pants = clothesStore.getClothingItemById(outfit.bottomid);
+
+				// Return outfit with full shirt and pants details
+				console.log(savedOutfits);
+				return {
+					...outfit,
+					shirt,
+					pants
+				};
+			});
+		});
 	});
 
 	// Toggle through shirts
@@ -60,7 +76,6 @@
 	}
 
 	function previousPant() {
-		console.log(pants.length);
 		if (pants.length) {
 			currentPantIndex = (currentPantIndex - 1 + pants.length) % pants.length;
 			selectedPant = pants[currentPantIndex];
@@ -70,31 +85,29 @@
 	// Save the outfit
 	function saveOutfit() {
 		if (outfitName && selectedShirt && selectedPant) {
-			outfitStore.addOutfit(outfitName, selectedShirt.id, selectedPant.id, []);
+			outfitStore.addOutfit(outfitName, selectedShirt.id, selectedPant.id, []); // Add the outfit to the store
 			alert(`Outfit "${outfitName}" saved!`);
 			outfitName = ''; // Clear outfit name after saving
 		} else {
 			alert('Please enter an outfit name and select a shirt and pants.');
 		}
+		console.log(get(outfitStore));
 	}
 </script>
+
 <div class="components">
 	<Tabs activeTab="1">
 		<svelte:fragment slot="labels">
 			<TabLabel tabnum="1">Create Outfit</TabLabel>
-			<TabLabel tabnum="2">View Outfits</TabLabel>
+			<TabLabel tabnum="2">Saved Outfits</TabLabel>
 		</svelte:fragment>
 		<svelte:fragment slot="contents">
 			<TabContent tabnum="1">
-				<div class="header_container">
-					<Header>Custom Outfits</Header>
-					<Button>View Saved Outfits</Button>
-				</div>
-			
 				<div class="body">
+					<!-- Select Shirt Section -->
 					<div class="select_item">
 						<div class="select_shirt">
-							<Header type="subheader" class="select_shirt_header">Select Shirt</Header>
+							<Header type="subheader">Select Shirt</Header>
 							<div class="carousel">
 								<Button on:click={previousShirt}>Previous Shirt</Button>
 								{#if selectedShirt}
@@ -104,10 +117,11 @@
 							</div>
 						</div>
 					</div>
-			
+
+					<!-- Select Pants Section -->
 					<div class="select_item">
 						<div class="select_pants">
-							<Header type="subheader" class="select_pants_header">Select Pants</Header>
+							<Header type="subheader">Select Pants</Header>
 							<div class="carousel">
 								<Button on:click={previousPant}>Previous Pant</Button>
 								{#if selectedPant}
@@ -117,10 +131,11 @@
 							</div>
 						</div>
 					</div>
-			
+
+					<!-- Save Outfit Section -->
 					<div class="save_outfit">
-						<div style='display:flex; justify-content:center; padding:10px;'>
-							<div style="display:flexbox; width :50%;">
+						<div>
+							<div style="width: 500px;">
 								<TextInput placeholder="Enter Outfit Name" bind:value={outfitName} />
 							</div>
 						</div>
@@ -130,15 +145,44 @@
 					</div>
 				</div>
 			</TabContent>
-			</svelte:fragment>
-		</Tabs>
+
+			<!-- Saved Outfits Tab -->
+			<TabContent tabnum="2">
+				{#if savedOutfits}
+					{#each savedOutfits as outfit (outfit.id)}
+						<div class="accordion">
+							<Accordion>
+								<AccordionHeader slot="header">
+									<Text>{outfit.name}</Text>
+								</AccordionHeader>
+								<AccordionBody slot="body">
+									<div class="outfit-display">
+										<div class="outfit-shirt">
+											<Text type="light">{outfit.shirt.name}</Text>
+											<div class="accordion-img">
+												<img class="shirt-img" src={outfit.shirt.img} alt="shirt" />
+											</div>
+										</div>
+										<div class="outfit-pants">
+											<Text type="light">{outfit.pants.name}</Text>
+											<div class="accordion-img">
+												<img class="pant-img" src={outfit.pants.img} alt="shirt" />
+											</div>
+										</div>
+									</div>
+								</AccordionBody>
+							</Accordion>
+						</div>
+					{/each}
+				{:else}
+					<p>No saved outfits yet.</p>
+				{/if}
+			</TabContent>
+		</svelte:fragment>
+	</Tabs>
 </div>
 
-
-
 <style>
-	@import '/src/variables.css';
-
 	.components {
 		display: flex;
 		flex-direction: column;
@@ -146,27 +190,29 @@
 		gap: 16px;
 	}
 
-	.header_container {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		width: 100%;
-	}
-
 	.body {
 		display: flex;
 		flex-direction: column;
-		justify-content: space-between;
+		align-items: center;
 		width: 100%;
 		gap: 16px;
 	}
-
 	.select_shirt,
 	.select_pants {
 		flex-direction: column;
 		padding: 6px;
 		outline-style: solid;
 		outline-color: var(--color-green);
+		height: 280px;
+		width:400px;
+	}
+
+	
+	.select_shirt img,
+	.select_pants img {
+		height: 250px;
+		width: 200px;
+		
 	}
 
 	.select_item {
@@ -179,19 +225,33 @@
 		flex-direction: column;
 	}
 
-	.select_shirt_header,
-	.select_pants_header {
-		display: flex;
-		width: 100%;
-		flex-direction: column;
-		text-align: center;
-		margin:20px;
-	}
-
 	.carousel {
 		display: flex;
 		justify-content: space-between;
-		align-items:end;
 		width: 100%;
+		align-items: end;
 	}
+
+	.save_outfit {
+		display: flex;
+		flex-direction: column;
+	}
+	.outfit-display {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+	.outfit-pants, 
+	.outfit-shirt {
+		text-align: center;
+	}
+	.outfit-pants img,
+	.outfit-shirt img {
+		height:250px;
+		width: 175px;
+	}
+	.accordion-img {
+		height: max-content;
+	}
+
 </style>
