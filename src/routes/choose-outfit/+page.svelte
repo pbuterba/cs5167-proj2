8733c06fd12fcbba1e19696d91dd9864b49fe271
Nate-Louder <nmlouder@gmail.com>
2024-10-17 17:34:57 -0400

    fixed tab change issue

--- src/routes/choose-outfit/+page.svelte
+++ src/routes/choose-outfit/+page.svelte
@@ -1,20 +1,26 @@
 <script>
-    import Header from '$lib/components/Header/Header.svelte';
-    import Button from '$lib/components/Button/Button.svelte';
-    import Popover from '$lib/components/Popover/Popover.svelte';
-    import PopoverMultiSelectContent from '$lib/components/Popover/CustomPopoverContent/PopoverMultiSelectContent.svelte';
+	import Header from '$lib/components/Header/Header.svelte';
+	import Button from '$lib/components/Button/Button.svelte';
+	import Popover from '$lib/components/Popover/Popover.svelte';
+	import PopoverMultiSelectContent from '$lib/components/Popover/CustomPopoverContent/PopoverMultiSelectContent.svelte';
 	import PopoverChipTrigger from '$lib/components/Popover/CustomPopoverTrigger/PopoverChipTrigger.svelte';
-    import {outfitStore, clothesStore, weatherStore, selectedOutfitId} from '../../lib/utilities/stores';
-    import Tabs from '$lib/components/Tabs/Tabs.svelte';
+	import StandardContentLayout from '$lib/components/StandardContentLayout/StandardContentLayout.svelte';
+	import {
+		outfitStore,
+		clothesStore,
+		weatherStore,
+		selectedOutfitId
+	} from '../../lib/utilities/stores';
+	import Tabs from '$lib/components/Tabs/Tabs.svelte';
 	import TabContent from '$lib/components/Tabs/TabContent.svelte';
 	import TabLabel from '$lib/components/Tabs/TabLabel.svelte';
-    import Toast from '$lib/components/Toast/Toast.svelte'
-    
-    let popoverItems = [];
-    let filteredOutfits = $outfitStore;
-    let toast;
-	let activeTabNumber;
-    $: activeTabNumber = $selectedOutfitId === undefined ? "1":"2";
+	import Toast from '$lib/components/Toast/Toast.svelte';
+
+	let popoverItems = [];
+	let filteredOutfits = $outfitStore;
+	let toast;
+	let tabs;
+	let activeTabNumber = $selectedOutfitId === undefined ? '1' : '2';
 
 	// The DEFAULT temperature is 50 degrees unless specified to use current temp
 	let clothingFilters = { cozy: false, formal: false, temp: 50 };
@@ -55,81 +61,96 @@
 			loopCount = loopCount + 1;
 		});
 
-        if (filteredOutfits.length == 0) {
-            toast.addToast();
-        }
-    }
+		if (filteredOutfits.length == 0) {
+			toast.addToast();
+		}
+	}
 
 	function handlePopoverItemsChanged(event) {
 		popoverItems = event.detail.selectedItems;
-        filterOutfits();
+		filterOutfits();
 	}
-    
-    function submitOutfitChoice(outfitId) {
-        selectedOutfitId.update(() => outfitId);
-    }
 
-    filterOutfits();
+	function submitOutfitChoice(outfitId) {
+		selectedOutfitId.update(() => outfitId);
+		tabs.changeTab('2');
+	}
 
+	filterOutfits();
 </script>
 
 <div class="components">
-    <Tabs activeTab={activeTabNumber}>
-        <svelte:fragment slot="labels">
+	<Tabs activeTab={activeTabNumber} bind:this={tabs}>
+		<svelte:fragment slot="labels">
 			<TabLabel tabnum="1">Choose Outfit</TabLabel>
 			<TabLabel tabnum="2">Selected Outfit</TabLabel>
 		</svelte:fragment>
-        
-        <svelte:fragment slot="contents">
-        <TabContent tabnum="1">
-            <div class="box">
-                <Header type="h2">What type of outfit are you looking for today? </Header> <br>        
-                <div class="split_container">
-                    <div class="popover_content">
-                        <Popover on:popoverItemsChanged={handlePopoverItemsChanged}>
-                            <PopoverChipTrigger class="popover_content" slot="trigger" label="Preferences" />
-                            <PopoverMultiSelectContent class="popover_content" slot="content" items={preferenceItems} />
-                        </Popover>
-                    </div>
-                </div>
-            </div>
 
-			<br> <br>
-            <div class="outfit_cards">
-                {#each $outfitStore as outfit}
-                    {#if filteredOutfits.includes(outfit)}
-                    <div class="display_outfit">
-                        <Header type="h2">{outfit.name} </Header>          
-						<div class="img_wrapper">
-							<img src={clothesStore.getClothingItemById(outfit.topid).img} alt="top"/>
-							<img src={clothesStore.getClothingItemById(outfit.bottomid).img} alt="bottom"/>
+		<svelte:fragment slot="contents">
+			<TabContent tabnum="1">
+				<StandardContentLayout>
+					<div class="box">
+						<Header type="h2">What type of outfit are you looking for today?</Header>
+						<div class="split_container">
+							<div class="popover_content">
+								<Popover on:popoverItemsChanged={handlePopoverItemsChanged}>
+									<PopoverChipTrigger class="popover_content" slot="trigger" label="Preferences" />
+									<PopoverMultiSelectContent
+										class="popover_content"
+										slot="content"
+										items={preferenceItems}
+									/>
+								</Popover>
+							</div>
 						</div>
-						<Button on:click={()=>submitOutfitChoice(outfit.id)}>Submit Outfit Choice</Button>
-                    </div>
-					<br>
-                    {/if}
-                {/each}
-            </div>
-        </TabContent>
+					</div>
+					<div class="outfit_cards">
+						{#each $outfitStore as outfit}
+							{#if filteredOutfits.includes(outfit)}
+								<div class="display_outfit">
+									<Header type="subheader">{outfit.name}</Header>
+									<div class="img_wrapper">
+										<img src={clothesStore.getClothingItemById(outfit.topid).img} alt="top" />
+										<img src={clothesStore.getClothingItemById(outfit.bottomid).img} alt="bottom" />
+									</div>
+									<Button on:click={() => submitOutfitChoice(outfit.id)}>Choose this Outfit</Button>
+								</div>
+							{/if}
+						{/each}
+					</div>
+				</StandardContentLayout>
+			</TabContent>
 
-        <TabContent tabnum="2">
-            {#if $selectedOutfitId != undefined}
-                <Header type="h1">Selected Outfit: </Header>
-                <div class="display_outfit">
-                    <Header type="h2">{outfitStore.getOutfitById($selectedOutfitId).name} </Header>
-                    <div class="img_wrapper">
-                        <img src={clothesStore.getClothingItemById(outfitStore.getOutfitById($selectedOutfitId).topid).img} alt="top"/>
-                        <img src={clothesStore.getClothingItemById(outfitStore.getOutfitById($selectedOutfitId).bottomid).img} alt="bottom"/>
-                    </div>
-                </div>  
-            {/if} 
-        </TabContent>
-        </svelte:fragment>
+			<TabContent tabnum="2">
+				{#if $selectedOutfitId != undefined}
+					<StandardContentLayout>
+						<Header type="h1">Selected Outfit</Header>
+						<div class="display_outfit full">
+							<Header type="h2">{outfitStore.getOutfitById($selectedOutfitId).name}</Header>
+							<div class="img_wrapper">
+								<img
+									src={clothesStore.getClothingItemById(
+										outfitStore.getOutfitById($selectedOutfitId).topid
+									).img}
+									alt="top"
+								/>
+								<img
+									src={clothesStore.getClothingItemById(
+										outfitStore.getOutfitById($selectedOutfitId).bottomid
+									).img}
+									alt="bottom"
+								/>
+							</div>
+						</div>
+					</StandardContentLayout>
+				{/if}
+			</TabContent>
+		</svelte:fragment>
 	</Tabs>
 	<Toast
 		title="Incorrect Preferences"
-		message='There are no outfits that match your preferences. Please select a new preference combination.'
-		type='error'
+		message="There are no outfits that match your preferences. Please select a new preference combination."
+		type="error"
 		bind:this={toast}
 	/>
 </div>
@@ -143,33 +164,23 @@
 		gap: 16px;
 	}
 
-	.split_container {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		width: 100%;
-		padding: 5%;
-		border-radius: 40px;
-		box-sizing: border-box;
-	}
-
 	.checkboxes {
 		display: flex;
 		flex-direction: row;
 	}
 
 	.box {
-		background-color: var(--color-surface-darker);
-		border: 5px solid var(--color-navy-dark);
+		display: flex;
+		justify-content: space-between;
 		border-radius: 20px;
-		padding: 3%;
+		padding: 8px;
 		height: fit-content;
 	}
 
 	.img_wrapper img {
 		border-radius: 8px;
 		border: 3px solid var(--color-edge);
-		padding: 16px;
+		padding: 4px;
 		width: 200px;
 		height: 200px;
 		object-fit: cover;
@@ -181,7 +192,7 @@
 		gap: 16px;
 		align-items: center;
 		justify-content: center;
-		width: 100%;
+		width: fit-content;
 		border-radius: 8px;
 		box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.1);
 		padding: 16px;
@@ -190,5 +201,17 @@
 
 	.popover_content {
 		padding-top: 0%;
+	}
+
+	.outfit_cards {
+		display: flex;
+		flex-direction: row;
+		gap: 16px;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+
+	.full {
+		width: 100%;
 	}
 </style>
